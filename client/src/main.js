dc318135b7da6bc5941548e68af20ac881cb65f1
Gildeon <gildeon.muniz@gmail.com>
2025-11-02 18:22:01 -0300

    configuracao pwa

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,24 @@ import Pacientes from './views/Pacientes.vue'
 import NovaFicha from './views/NovaFicha.vue'
 import DetalhePaciente from './views/DetalhePaciente.vue'
 
+// Registro do Service Worker (PWA)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registrado com sucesso:', registration.scope)
+
+        // Verificar atualizações periodicamente
+        setInterval(() => {
+          registration.update()
+        }, 60000) // A cada 1 minuto
+      })
+      .catch(error => {
+        console.error('Erro ao registrar Service Worker:', error)
+      })
+  })
+}
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
